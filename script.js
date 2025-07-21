113f47ebe98c9504c25b240f8e593e3f790ae9e5
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-21 10:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.querySelector("#submit").addEventListener("click",()=>{
+let Fname = document.querySelector("#FirstName").value
+let Lname = document.querySelector("#LastName").value
+let Pnumber = document.querySelector("#PhoneNumber").value
+let email = document.querySelector("#EmailID").value
+	alert(`First Name: ${Fname} Last Name: ${Lname} Phone Number: ${Pnumber} Email ID: ${email}`)
+})
