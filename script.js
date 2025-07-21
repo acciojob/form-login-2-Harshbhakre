375b569b785d82c89975b5a082d88c86ef7ded3f
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-21 10:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let Fname = document.querySelector("#FirstName").value
 let Lname = document.querySelector("#LastName").value
 let Pnumber = document.querySelector("#PhoneNumber").value
 let email = document.querySelector("#EmailID").value
-	alert(`First Name: ${Fname} Last Name: ${Lname} Phone Number: ${Pnumber} Email ID: ${email}`)
+	alert(`First Name: ${Fname} \nLast Name: ${Lname} \nPhone Number: ${Pnumber} \nEmail ID: ${email}`)
 })
